a06cde359a625d243bb08d72ac1bd8f76d12348c
Tim Smart <tim.smart@arisechurch.com>
2022-09-17 19:31:03 +1200

    Document pubic facing internal API's

--- packages/nucleus/lib/src/internal/defs.dart
+++ packages/nucleus/lib/src/internal/defs.dart
@@ -6,8 +6,13 @@ typedef OnSetSelf = void Function();
 typedef OnDispose = void Function(void Function());
 typedef AssertNotDisposed = void Function(String method);
 typedef AddParent = void Function(Node node);
+
+/// Represents a function that retrieves an [Atom]'s value.
 typedef GetAtom = A Function<A>(Atom<A> atom);
+
+/// Represents a function that sets a [WritableAtom]'s value.
 typedef SetAtom = void Function<R, W>(WritableAtom<R, W> atom, W value);
+
 typedef SetSelf<A> = void Function(A value);
 
 typedef ValueBuilder<A> = A Function();
--- packages/nucleus/lib/src/internal/registry.dart
+++ packages/nucleus/lib/src/internal/registry.dart
@@ -5,6 +5,10 @@ import 'package:nucleus/nucleus.dart';
 
 import 'internal.dart';
 
+/// Responsible for mapping atom's to their state.
+///
+/// Each atom corresponds to a [Node], which contains the current state for that
+/// atom.
 class AtomRegistry {
   AtomRegistry({
     List<AtomInitialValue> initialValues = const [],
@@ -15,16 +19,24 @@ class AtomRegistry {
     }
   }
 
-  final scheduler = Scheduler();
+  final _scheduler = Scheduler();
+
+  /// The state map, where each atom has a corresponding [Node].
   final nodes = HashMap<Atom, Node>();
 
+  /// Retrieve the state for the given [Atom], creating or rebuilding it when
+  /// required.
   A get<A>(Atom<A> atom) => _ensureNode(atom).value as A;
 
+  /// Set the state of a [WritableAtom].
   void set<R, W>(WritableAtom<R, W> atom, W value) {
     final node = _ensureNode(atom);
     atom.write(get, set, node.setValue, value);
   }
 
+  /// Listen to changes of an atom's state.
+  ///
+  /// Call [get] to retrieve the latest value after the [handler] is called.
   void Function() subscribe<A>(
     Atom<A> atom,
     void Function() handler, {
@@ -44,21 +56,26 @@ class AtomRegistry {
     return () {
       remove();
       if (node.canBeRemoved) {
-        scheduler.runPostFrame(() => _maybeRemoveNode(node));
+        _scheduler.runPostFrame(() => _maybeRemoveNode(node));
       }
     };
   }
 
-  Future<A> use<A>(Atom atom, FutureOr<A> Function() f) async {
+  /// Listen to an [atom], run the given [fn] (which can return a [Future]),
+  /// then remove the listener once the [fn] is complete.
+  Future<A> use<A>(Atom atom, FutureOr<A> Function() fn) async {
     final remove = subscribe(atom, () {});
 
     try {
-      return await f();
+      return await fn();
     } finally {
       remove();
     }
   }
 
+  /// Listen to an [atom], but don't register a handler function.
+  ///
+  /// Returns a function which 'unmounts' the [atom].
   void Function() mount(Atom atom) => subscribe(atom, () {
         get(atom);
       });
@@ -67,7 +84,7 @@ class AtomRegistry {
 
   Node _ensureNode(Atom atom) => nodes.putIfAbsent(atom, () {
         if (!atom.shouldKeepAlive) {
-          scheduler.runPostFrame(() => _maybeRemoveAtom(atom));
+          _scheduler.runPostFrame(() => _maybeRemoveAtom(atom));
         }
         return Node(atom, _createNodeDepsFn(atom));
       });
