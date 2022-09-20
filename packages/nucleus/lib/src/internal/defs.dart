bf3ec1ef7b9a65f6ce7751e1ce41a23552a0a4de
Tim Smart <tim.smart@arisechurch.com>
2022-09-20 15:00:12 +1200

    remove unused defs

--- packages/nucleus/lib/src/internal/defs.dart
+++ packages/nucleus/lib/src/internal/defs.dart
@@ -1,20 +1,10 @@
 part of 'internal.dart';
 
-typedef OnSetSelf = void Function();
-typedef OnDispose = void Function(void Function());
-typedef AssertNotDisposed = void Function(String method);
-typedef AddParent = void Function(Node node);
-typedef GetPreviousValue<A> = A? Function();
-
 /// Represents a function that retrieves an [Atom]'s value.
 typedef GetAtom = A Function<A>(Atom<A> atom);
 
 /// Represents a function that sets a [WritableAtom]'s value.
 typedef SetAtom = void Function<R, W>(WritableAtom<R, W> atom, W value);
 
+/// Represents function that sets the current atom's value
 typedef SetSelf<A> = void Function(A value);
-
-typedef SubscribeToAtom = void Function() Function(
-  Atom atom,
-  void Function() handler,
-);
