41c8f0c653446df4076990f6ae008c0daa47c16d
Tim Smart <tim.smart@arisechurch.com>
2022-09-22 09:42:38 +1200

    Add atom GC to AtomBuilder

--- packages/flutter_nucleus/lib/src/atom_builder.dart
+++ packages/flutter_nucleus/lib/src/atom_builder.dart
@@ -21,23 +21,39 @@ class AtomBuilder extends StatefulWidget {
   State<AtomBuilder> createState() => _AtomBuilderState();
 }
 
+typedef _CancelMap = HashMap<Atom, void Function()>;
+
 class _AtomBuilderState extends State<AtomBuilder> {
   late var _registry = AtomScope.registryOf(context);
-  final _cancellers = HashMap<Atom, void Function()>();
   final _valueCache = HashMap<Atom, Object?>();
+
+  _CancelMap? _previousMounts;
+  _CancelMap _mounts = HashMap();
+
+  _CancelMap? _previousSubscriptions;
+  _CancelMap _subscriptions = HashMap();
 
   A _watch<A>(
     Atom<A> atom, {
     bool listen = true,
   }) {
-    if (!_cancellers.containsKey(atom)) {
-      if (listen) {
-        _cancellers[atom] = _registry.subscribe(
+    if (listen && !_subscriptions.containsKey(atom)) {
+      if (_previousSubscriptions != null &&
+          _previousSubscriptions!.containsKey(atom)) {
+        _subscriptions[atom] = _previousSubscriptions![atom]!;
+        _previousSubscriptions!.remove(atom);
+      } else {
+        _subscriptions[atom] = _registry.subscribe(
           atom,
           () => setState(() => _valueCache.remove(atom)),
         );
+      }
+    } else if (!_mounts.containsKey(atom)) {
+      if (_previousMounts != null && _previousMounts!.containsKey(atom)) {
+        _mounts[atom] = _previousMounts![atom]!;
+        _previousMounts!.remove(atom);
       } else {
-        _cancellers[atom] = _registry.mount(atom);
+        _mounts[atom] = _registry.mount(atom);
       }
     }
 
@@ -45,8 +61,33 @@ class _AtomBuilderState extends State<AtomBuilder> {
   }
 
   @override
-  Widget build(BuildContext context) =>
-      widget.builder(context, _watch, widget.child);
+  Widget build(BuildContext context) {
+    _previousSubscriptions = _subscriptions;
+    _subscriptions = HashMap();
+    _previousMounts = _mounts;
+    _mounts = HashMap();
+
+    final result = widget.builder(context, _watch, widget.child);
+
+    if (_previousSubscriptions!.isNotEmpty) {
+      for (final e in _previousSubscriptions!.entries) {
+        e.value();
+        _valueCache.remove(e.key);
+      }
+    }
+
+    if (_previousMounts!.isNotEmpty) {
+      for (final e in _previousMounts!.entries) {
+        e.value();
+        _valueCache.remove(e.key);
+      }
+    }
+
+    _previousSubscriptions = null;
+    _previousMounts = null;
+
+    return result;
+  }
 
   @override
   void didChangeDependencies() {
@@ -60,10 +101,16 @@ class _AtomBuilderState extends State<AtomBuilder> {
   }
 
   void _dispose() {
-    for (final cancel in _cancellers.values) {
+    for (final cancel in _subscriptions.values) {
       cancel();
     }
-    _cancellers.clear();
+    _subscriptions.clear();
+
+    for (final cancel in _mounts.values) {
+      cancel();
+    }
+    _mounts.clear();
+
     _valueCache.clear();
   }
 
