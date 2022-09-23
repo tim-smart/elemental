93a93f6652bc99fc60baf67e8810ddb78e4d59c6
Tim Smart <tim.smart@arisechurch.com>
2022-09-23 13:33:47 +1200

    Test select rebuild counts

--- packages/nucleus/test/atoms/extension_test.dart
+++ packages/nucleus/test/atoms/extension_test.dart
@@ -32,5 +32,35 @@ void main() {
         equals(['Tim', 'John']),
       );
     });
+
+    test('does not rebuild if another property changes', () async {
+      final userAtom = stateAtom({
+        'id': 1,
+        'name': 'Tim',
+      });
+      final idAtom = userAtom.select((user) => user['id'] as int);
+      final nameAtom = userAtom.select((value) => value['name'] as String);
+
+      final registry = AtomRegistry();
+
+      var idValues = <int>[];
+      var nameValues = <String>[];
+      registry.subscribe(
+        idAtom,
+        (int id) => idValues.add(id),
+        fireImmediately: true,
+      );
+      registry.subscribe(
+        nameAtom,
+        (String id) => nameValues.add(id),
+        fireImmediately: true,
+      );
+
+      registry.set(userAtom, {'id': 2, 'name': 'Tim'});
+      registry.set(userAtom, {'id': 2, 'name': 'John'});
+
+      expect(idValues, equals([1, 2]));
+      expect(nameValues, equals(['Tim', 'John']));
+    });
   });
 }
