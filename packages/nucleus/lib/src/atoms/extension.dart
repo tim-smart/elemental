3e284260b2d045d672b3918f87b1485bfd86e49b
Tim Smart <tim.smart@arisechurch.com>
2022-09-23 22:38:43 +1200

    DRY in asyncSelect

--- packages/nucleus/lib/src/atoms/extension.dart
+++ packages/nucleus/lib/src/atoms/extension.dart
@@ -3,6 +3,8 @@ part of '../atoms.dart';
 extension AtomExtension<A> on Atom<A> {
   /// Create a derived atom, that transforms an atoms value using the given
   /// function [f].
+  ///
+  /// Only rebuilds when the selected value changes.
   Atom<B> select<B>(B Function(A value) f) =>
       ReadOnlyAtom((get) => f(get(this)));
 }
@@ -10,6 +12,8 @@ extension AtomExtension<A> on Atom<A> {
 extension FutureValueAtomExtension<A> on Atom<FutureValue<A>> {
   /// Create a derived atom, that transforms an atoms value using the given
   /// function [f].
+  ///
+  /// Only rebuilds when the selected value changes.
   Atom<FutureValue<B>> select<B>(B Function(A value) f) => ReadOnlyAtom((get) {
         final value = get(this).map(f);
         if (value.dataOrNull != null) {
@@ -26,20 +30,13 @@ extension FutureValueAtomExtension<A> on Atom<FutureValue<A>> {
 
   /// Create a derived atom, that transforms an atoms value using the given
   /// function [f].
-  Atom<Future<B>> asyncSelect<B>(B Function(A value) f) =>
-      atomWithParent(ReadOnlyAtom<B?>((get) {
-        final value = get(this).map(f);
-        if (value.dataOrNull != null) {
-          return value.dataOrNull;
-        }
-
-        if (get.previousValue != null) {
-          return get.previousValue;
-        }
-
-        return null;
-      }), (get, Atom<B?> parent) {
-        final value = get(parent);
-        return value == null ? Future.any([]) : Future.value(value);
-      });
+  ///
+  /// Only rebuilds when the selected value changes.
+  Atom<Future<B>> asyncSelect<B>(B Function(A value) f) => atomWithParent(
+        select(f),
+        (get, Atom<FutureValue<B>> parent) => get(parent).whenOrElse(
+          data: Future.value,
+          orElse: () => Future.any([]),
+        ),
+      );
 }
