b2883f2cbfe587fa02a79afcc1eac3bc986c96a2
Tim Smart <tim.smart@arisechurch.com>
2022-09-26 13:39:17 +1300

    Use SchedulerBinding for flutter postFrame callbacks

--- packages/flutter_nucleus/lib/src/atom_scope.dart
+++ packages/flutter_nucleus/lib/src/atom_scope.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_nucleus/flutter_nucleus.dart';
+// ignore: implementation_imports
+import 'package:nucleus/src/internal/internal.dart' as internal;
 
 class AtomScope extends InheritedWidget {
-  static final defaultRegistry = AtomRegistry();
-
   AtomScope({
     super.key,
     required super.child,
     List<AtomInitialValue> initialValues = const [],
-  }) : registry = AtomRegistry(initialValues: initialValues);
+  }) : registry = AtomRegistry(
+          initialValues: initialValues,
+          scheduler: _buildScheduler(),
+        );
+
+  static final defaultRegistry = AtomRegistry(scheduler: _buildScheduler());
+
+  static internal.Scheduler _buildScheduler() {
+    final binding = SchedulerBinding.instance;
+
+    void postFrame(void Function() task) =>
+        binding.addPostFrameCallback((timeStamp) => task());
+
+    return internal.Scheduler(postFrameRunner: postFrame);
+  }
 
   final AtomRegistry registry;
 
