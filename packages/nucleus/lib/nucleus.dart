3271405a087d4eb9971ff3d1d914e67edb50ef7f
Tim Smart <tim.smart@arisechurch.com>
2022-09-29 16:13:04 +1300

    Hide Tuple classes from exports

--- packages/nucleus/lib/nucleus.dart
+++ packages/nucleus/lib/nucleus.dart
@@ -3,4 +3,4 @@ library nucleus;
 export 'src/atoms.dart';
 export 'src/internal/internal.dart'
     show AtomRegistry, GetAtom, SetAtom, SetSelf;
-export 'src/utils.dart';
+export 'src/utils.dart' hide Tuple2, Tuple3;
