e82a07d3e97152b3aca7d876189bf0c2de646566
Tim Smart <hello@timsmart.co>
2023-02-20 13:27:49 +1300

    feat: refAtomWith*

--- packages/elemental/lib/src/atoms.dart
+++ packages/elemental/lib/src/atoms.dart
@@ -10,3 +10,36 @@ ReadOnlyAtom<Ref<A>> refAtom<A>(A a) => atom((get) {
       });
       return Ref.makeScope(a).provide(scope).runSync();
     });
+
+AtomWithParent<A, Atom<Ref<A>>> refAtomWithParent<A>(A a) =>
+    atomWithParent(refAtom(a), (get, parent) {
+      final ref = get(parent);
+
+      get.onDispose(ref.stream.listen((a) {
+        get.setSelf(a);
+      }).cancel);
+
+      return ref.get.runSync();
+    });
+
+ReadOnlyAtom<Ref<A>> refAtomWithStorage<A>(
+  A defaultValue, {
+  required String key,
+  required A Function(dynamic) fromJson,
+  required dynamic Function(A) toJson,
+  required Atom<NucleusStorage> storage,
+}) =>
+    atomWithStorage<Ref<A>, A>((get, read, write) {
+      final scope = Scope.closable();
+      get.onDispose(() {
+        scope.closeScope.run();
+      });
+
+      final ref =
+          Ref.makeScope(read() ?? defaultValue).provide(scope).runSync();
+      get.onDispose(ref.stream.listen((a) {
+        write(a);
+      }).cancel);
+
+      return ref;
+    }, key: key, fromJson: fromJson, toJson: toJson, storage: storage);
