e4addaf9c336737e338431e6cac5b07c22d1f81e
Tim Smart <hello@timsmart.co>
2023-02-23 15:40:19 +1300

    chore: improve zioRefAtomSync error message

--- packages/elemental/lib/src/atoms.dart
+++ packages/elemental/lib/src/atoms.dart
@@ -4,7 +4,13 @@ final runtimeAtom = atom((get) => Runtime.defaultRuntime);
 
 AtomWithParent<A, Atom<Ref<A>>> zioRefAtomSync<E, A>(EIO<E, Ref<A>> zio) =>
     atomWithParent(
-      atom((get) => get(runtimeAtom).runSyncOrThrow(zio)),
+      atom((get) {
+        try {
+          return get(runtimeAtom).runSyncOrThrow(zio);
+        } on Interrupted {
+          throw "zioRefAtomSync: Could not access Ref. Maybe a Layer has not been built yet?";
+        }
+      }),
       (get, parent) {
         final ref = get(parent);
 
