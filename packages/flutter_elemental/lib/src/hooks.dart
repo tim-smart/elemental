fb2c2c370733eb351c695f2e9e5670dcd4beab04
Tim Smart <tim.smart@arisechurch.com>
2023-02-25 09:54:37 +1300

    refactor: optional hook keys

--- packages/flutter_elemental/lib/src/hooks.dart
+++ packages/flutter_elemental/lib/src/hooks.dart
@@ -4,27 +4,27 @@ import 'package:flutter_elemental/flutter_elemental.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 FutureOr<Exit<E, A>> Function() useZIO<E, A>(
-  EIO<E, A> zio,
-  List<Object?> keys,
-) {
+  EIO<E, A> zio, [
+  List<Object?> keys = const [],
+]) {
   final context = useContext();
   final runtime = context.getAtom(runtimeAtom);
   return useCallback(() => runtime.run(zio), keys);
 }
 
 FutureOr<Exit<E, A>> Function(T _) useZIO1<E, A, T>(
-  EIO<E, A> Function(T _) f,
-  List<Object?> keys,
-) {
+  EIO<E, A> Function(T _) f, [
+  List<Object?> keys = const [],
+]) {
   final context = useContext();
   final runtime = context.getAtom(runtimeAtom);
   return useCallback((_) => runtime.run(f(_)), keys);
 }
 
 FutureOr<Exit<E, A>> Function(T t, U u) useZIO2<E, A, T, U>(
-  EIO<E, A> Function(T t, U u) f,
-  List<Object?> keys,
-) {
+  EIO<E, A> Function(T t, U u) f, [
+  List<Object?> keys = const [],
+]) {
   final context = useContext();
   final runtime = context.getAtom(runtimeAtom);
   return useCallback((t, u) => runtime.run(f(t, u)), keys);
